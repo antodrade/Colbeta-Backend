ae606f5aa64458bd4729005837c583f7eb0f6baa
antodrade <antoandrade1988@gmail.com>
2026-07-01 21:32:18 -0500

    agregando la politica cors para el authCOntroller

--- src/main/java/gm/Colbeta/controlador/AuthController.java
+++ src/main/java/gm/Colbeta/controlador/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://localhost:4200/", "http://colbeta-frontend2.onrender.com"})
+@CrossOrigin(origins = {"http://localhost:4200/", "https://colbeta-frontend2.onrender.com"})
 public class AuthController {
 
     @Autowired
