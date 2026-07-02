74e5a640b2e47c524fbb2590eedbb0dc8ac55dd9
antodrade <antoandrade1988@gmail.com>
2026-07-01 21:11:13 -0500

    arrerglado error de http a htttps

--- src/main/java/gm/Colbeta/config/SecurityConfig.java
+++ src/main/java/gm/Colbeta/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http//colbeta-frontend2.onrender.com"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https//colbeta-frontend2.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST","PUT", "DELETE","OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
